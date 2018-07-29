1a8273525d47db41ae0652170a70fd45566b73a1
SK Z <718114245@qq.com>
2018-07-29 22:29:13 +0800

    在OnActionExecuting中添加Access-Control-Allow-Origin头信息，允许跨域请求

---
+++ WebCore/Fileters/MyResourceFilterAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCore.Fileters
+{
+    public class MyResourceFilterAttribute : IResourceFilter
+    {
+        private static Dictionary<string, object> _cache = new Dictionary<string, object>();
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            Console.WriteLine("资源请求前");
+            context.Result = new ContentResult
+            {
+                Content = "资源请求前适合做缓存"
+            };
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            Console.WriteLine("资源请求后");
+        }
+    }
+}
