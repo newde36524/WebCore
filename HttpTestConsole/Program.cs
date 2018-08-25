9a0f7fbdfd81c6c29d93f08439e3768559a0b8ab
SK Z <718114245@qq.com>
2018-08-25 20:21:21 +0800

    创建TestController，提供HttpClient模拟Http访问的控制台应用程序。

---
+++ HttpTestConsole/Program.cs
@@ -0,0 +1,170 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace HttpTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 普通Get请求
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = "http://localhost:51582/TestController/GetResult";
+                httpClient.GetAsync(url).ContinueWith(x =>
+                {
+                    x.Result.Content.ReadAsStringAsync().ContinueWith(r =>
+                    {
+                        Console.WriteLine(r.Result);
+                    });
+                }).GetAwaiter().GetResult();
+            }
+
+            #endregion
+
+            #region Get带参数
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = "http://localhost:51582/TestController/GetReultWithParam";
+                httpClient.GetAsync(url).ContinueWith(x =>
+                {
+                    x.Result.Content.ReadAsStringAsync().ContinueWith(r =>
+                    {
+                        Console.WriteLine(r.Result);
+                    });
+                }).GetAwaiter().GetResult();
+            }
+
+            #endregion
+
+            #region 普通Post请求
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = "http://localhost:51582/TestController/PostResult";
+                httpClient.PostAsync(url, null).ContinueWith(x =>
+                {
+                    x.Result.Content.ReadAsStringAsync().ContinueWith(r =>
+                    {
+                        Console.WriteLine(r.Result);
+                    });
+                }).GetAwaiter().GetResult();
+            }
+
+            #endregion
+
+            #region Post带参数
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                //FormUrlEncodedContent 和 MultipartFormDataContent 两种方式模拟表单提交
+                string url = "http://localhost:51582/TestController/PostResultWithParam";
+
+                {//方式一
+                    var multipart = new MultipartFormDataContent();
+                    var msg = new StringContent("123");
+                    var msg2 = new StringContent("456");
+                    multipart.Add(msg, "msg");
+                    multipart.Add(msg2, "msg2");
+                    httpClient.PostAsync(url, multipart).ContinueWith(x =>
+                    {
+                        x.Result.Content.ReadAsStringAsync().ContinueWith(r =>
+                        {
+                            Console.WriteLine(r.Result);
+                        });
+                    }).GetAwaiter().GetResult();
+                }
+
+                {//方式二
+                    httpClient.PostAsync(url, new FormUrlEncodedContent(new Dictionary<string, string>() {
+                        {"msg","123" },
+                        {"msg2","456" }
+                    })).ContinueWith(x =>
+                    {
+                        x.Result.Content.ReadAsStringAsync().ContinueWith(r =>
+                        {
+                            Console.WriteLine(r.Result);
+                        });
+                    }).GetAwaiter().GetResult();
+                }
+            }
+
+            #endregion
+
+            #region 带参数Post提交 body，服务端直接匹配model
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = "http://localhost:51582/TestController/PostResultWithModel";
+
+                {//方式一 直接指定http头信息
+                    StreamContent streamContent = new StreamContent(new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { Id = 123, Name = "名字" }))));
+                    streamContent.Headers.Add("Content-Type", " application/json");
+                    httpClient.PostAsync(url, streamContent).ContinueWith(x =>
+                    {
+                        x.Result.Content.ReadAsStringAsync().ContinueWith(r =>
+                        {
+                            Console.WriteLine(r.Result);
+                        });
+                    }).GetAwaiter().GetResult();
+                }
+
+                {//方式二 需要先删除已有的默认http头信息
+                    StringContent stringContent = new StringContent(JsonConvert.SerializeObject(new { Id = 123, Name = "名字" }));
+                    stringContent.Headers.Remove("Content-Type");
+                    stringContent.Headers.Add("Content-Type", " application/json");
+                    httpClient.PostAsync(url, stringContent).ContinueWith(x =>
+                    {
+                        x.Result.Content.ReadAsStringAsync().ContinueWith(r =>
+                        {
+                            Console.WriteLine(r.Result);
+                        });
+                    }).GetAwaiter().GetResult();
+                }
+
+            }
+
+            #endregion
+
+            #region Post提交模拟Http文件上传,文件和普通参数同时提交
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = "http://localhost:51582/TestController/UploadFile";
+
+                MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+
+                var fileStream = new StreamContent(new MemoryStream(File.ReadAllBytes("a.jpg")));
+                fileStream.Headers.Add("Content-Type", "image/jpeg");
+                fileStream.Headers.Add("Content-Disposition", "form-data; name=\"file001\"; filename=\"a.jpg\"");
+                multipartFormDataContent.Add(fileStream, "file001", "a.jpg");
+
+                var fileStream2 = new StreamContent(new MemoryStream(File.ReadAllBytes("b.jpg")));
+                fileStream2.Headers.Add("Content-Type", "image/jpeg");//header值中的双引号要转义，不要用单引号代替，否则上传失败
+                fileStream2.Headers.Add("Content-Disposition", "form-data; name=\"file002\"; filename=\"b.jpg\"");
+                multipartFormDataContent.Add(fileStream2);
+
+                var stringContent = new StringContent("hello");
+                multipartFormDataContent.Add(stringContent, "msg");
+
+                httpClient.PostAsync(url, multipartFormDataContent).ContinueWith(x =>
+                {
+                    x.Result.Content.ReadAsStringAsync().ContinueWith(r =>
+                    {
+                        Console.WriteLine(r.Result);
+                    });
+                }).GetAwaiter().GetResult();
+            }
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebCore/WebApi/TestController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCore.WebApi
+{
+    [ApiController]
+    [Route("TestController")]
+    public class TestController : Controller
+    {
+        [HttpGet(nameof(GetResult))]
+        public string GetResult()
+        {
+            return "测试Get请求";
+        }
+
+        [HttpPost(nameof(PostResult))]
+        public string PostResult()
+        {
+            return "测试Post请求";
+        }
+
+        [HttpGet(nameof(GetReultWithParam))]
+        public string GetReultWithParam(string msg, string msg2)
+        {
+            return $"get {msg} {msg2}";
+        }
+
+        [HttpPost(nameof(PostResultWithParam))]
+        public string PostResultWithParam([FromForm]string msg, [FromForm]string msg2)
+        {
+            return $"post {msg} {msg2}";
+        }
+
+        [HttpPost(nameof(PostResultWithModel))]
+        public MyClass[] PostResultWithModel([FromBody]MyClass model)
+        {
+            return new [] { model };
+        }
+
+        [HttpPost(nameof(UploadFile))]
+        public string UploadFile([FromForm]IFormCollection form)
+        {
+            var file1 = form.Files["file001"];
+            var file2 = form.Files["file002"];
+            string msg = form["msg"];
+            return $"文件1长度：{file1.Length}  文件2长度:{file2.Length}  msg:{msg}";
+        }
+    }
+
+    public class MyClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
